4ccb449b35ec253e662ccf0418a976a42d227ab9
PauloR063 <Paulorcg063@gmail.com>
2025-07-31 00:15:27 -0300

    inicio da logica usando js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-const valorP = document.querySelector('.valor')
-
 function formatValue(value) {
     const formatador = value.toLocaleString('pt-BR', {
         style: 'currency',
@@ -7,3 +5,24 @@ function formatValue(value) {
     })
     return formatador
 }
+
+function showValue() {
+    const valorP = document.querySelector('.valor')
+    const valorUsers = document.querySelector('.valor-users')
+    const usersActive = document.querySelector('.users-active')
+    let totalUsers = 0
+
+    const totalValue = options.reduce((acc, value) => acc + value.ValordeCompra, 0);
+    const presenceFilter = options.filter(item => item.Presença === true)
+    const presenceActive = presenceFilter.reduce((acc) => acc + 1, 0)
+    options.forEach(() => {totalUsers++})
+
+
+    valorP.textContent = formatValue(totalValue)
+    valorUsers.textContent = totalUsers
+    usersActive.textContent = presenceActive
+    
+}
+
+showValue()
+
