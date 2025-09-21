2d4d71e92e2870041b94de29990528c070b5b15f
PauloR063 <Paulorcg063@gmail.com>
2025-09-21 08:39:41 -0300

    Atualização

--- script.js
+++ script.js
@@ -6,43 +6,12 @@ function formatValue(value) {
     return formatador
 }
 
-function showValue() {
-    const valorP = document.querySelector('.valor')
-    const valorUsers = document.querySelector('.valor-users')
-    const usersActive = document.querySelector('.users-active')
-    const receitaValor = document.querySelector('.receita-valor')
-    let totalUsers = 0
+const valorReceber = document.querySelector('#inputValueRecebido').value
 
-    const totalActive = options
-        .filter(usuario => usuario.Presença === true)
-        .reduce((soma, usuario) => soma + usuario.ValordeCompra, 0);
-    const totalValue = options.reduce((acc, value) => acc + value.ValordeCompra, 0);
-    const presenceFilter = options.filter(item => item.Presença === true)
-    const presenceActive = presenceFilter.reduce((acc) => acc + 1, 0)
-    options.forEach(() => {totalUsers++})
+function pegaValor(){
+    valorReceber.value
 
-    valorP.textContent = formatValue(totalActive)
-    valorUsers.textContent = totalUsers
-    usersActive.textContent = presenceActive
-    receitaValor.textContent = formatValue(totalValue)
-    
+    console.log(valorReceber)
 }
 
-function showName(){
-    const listName = document.querySelector('.uls-name')
-    const listPrecense = document.querySelector('.uls-precense')
-    const listValor = document.querySelector('.uls-valor')
-    let LiName = ''
-    let LiPresence = ''
-    let LiValor = ''
-    
-    
-    listName.innerHTML = LiName
-    listPrecense.innerHTML = LiPresence
-    listValor.innerHTML = LiValor
-    
-}
-
-showValue()
-showName()
-
+pegaValor()
