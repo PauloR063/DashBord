ba1bdbe94311d90bbfba8331bd406ca689dae9df
Paulo <Paulorcg063@gmail.com>
2025-09-24 22:41:19 -0300

    Adicionado a lista de transição

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const valorSalario = document.querySelector('#inputSalarioRecebido')
 const valorConta = document.querySelector('#inputValueRecebido')
+const BTNSalario = document.querySelector('.btn-salario')
+const BTNReceber = document.querySelector('.btn-receber')
+
 
 function formatValue(value) {
     const formatador = value.toLocaleString('pt-BR', {
@@ -13,29 +16,18 @@ function changeSalario(){
     const cardSalario = document.querySelector('.receita-valor')
 
     cardSalario.innerHTML = `${formatValue(parseFloat(valorSalario.value))}`
-}   
+
+    valorSalario.value = '';
+}
 
 function changeConta(){
     const cardConta = document.querySelector('.valor-users')
 
     cardConta.innerHTML = `${formatValue(parseFloat(valorConta.value))}`
+
+    valorConta.value = '';
 }
 
+BTNSalario.addEventListener('click', changeSalario)
+BTNReceber.addEventListener('click', changeConta)
 
-function enviarSalario(event){
-    if (event.key === 'Enter'){
-        changeSalario()
-        valorSalario.value = '';
-    }
-}
-
-function enviarConta(event){
-    if (event.key === 'Enter'){
-        changeConta()
-        valorConta.value = '';
-    } 
-}
-
-
-valorSalario.addEventListener('keydown', enviarSalario)
-valorConta.addEventListener('keydown', enviarConta)
